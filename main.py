45bfeee86b39ba312b7d0edc7b76b28b4a13f337
dnpatil86 <80618662+dnpatil86@users.noreply.github.com>
2021-03-14 14:07:57 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,22 +1,13 @@
 from selenium import webdriver
 import time
 import json
-
 path = "C:\webdrivers\chromedriver.exe"
 driver = webdriver.Chrome(path)
-
-
 driver.get("https://www.midsouthshooterssupply.com/dept/reloading/primers?currentpage=1")
-
-
-
 results = driver.find_elements_by_class_name("product")
-
 mylist = []
-
 for result in results:
     item = {}
-
     item["price"] = result.find_element_by_class_name("price").text
     item["title"] = result.find_element_by_class_name("catalog-item-name").text
     try:
@@ -27,9 +18,6 @@ for result in results:
     item["maftr"] = result.find_element_by_class_name("catalog-item-brand").text
 
     mylist.append(item)
-    ##print(item)
-
 jsondata = json.dumps(mylist)
 print (jsondata)
-
 driver.close()
